8474a46cf8f9841861121ca567a449cad038568b
Lwethu17 <olwethuradebe18@gmail.com>
2023-06-20 05:16:46 -0700

    Task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,16 @@
 
 void print_alphabet_x10(void)
 {
-	char ch;
+	char c;
 	int i;
 
 	while (i < 10)
 	{
-		ch = 'a';
-		while (ch <= 'z')
+		c = 'a';
+		while (c <= 'z')
 		{
-			_putchar(ch);
-			ch++;
+			_putchar(c);
+			c++;
 		}
 		_putchar('\n');
 		i++;
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowecase character
+ * @c: The character to be checked or 0 for anything else
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+}
