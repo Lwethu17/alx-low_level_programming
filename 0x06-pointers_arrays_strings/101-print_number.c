fcd139aa533c8d8000ea2a9ac12bd5b521f0afc7
Lwethu17 <olwethuradebe18@gmail.com>
2023-06-29 15:18:48 -0700

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,15 +8,19 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int n1;
+
+	n1 = n;
 
 	if (n < 0)
 	{
-	_putchar('-');
-	num = -num;
+		putchar('-');
+		n1 = -n;
 	}
 
-	if ((num / 10) > 0)
-	print_number(num / 10);
-       	_putchar((n1 % 10) + '0');
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	putchar((n1 % 10) + '0');
 }
--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * the program prints a[2] = 98, followed by a new line.
- *
- * Return: Always 0 (success)
- */
-
 int main(void)
-
 {
 	int n;
 	int a[5];
@@ -27,6 +18,6 @@ int main(void)
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2];
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
