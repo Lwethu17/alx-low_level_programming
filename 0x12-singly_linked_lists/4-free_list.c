3573f7eacdbd46356fd3045d91500ee44ec9abab
Lwethu17 <olwethuradebe18@gmail.com>
2023-07-28 08:44:47 -0700

    Task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,17 +3,17 @@
 
 /**
  * free_list - frees a linked list
- * @head: list_t list to be freed
+ * @head: list_t list that will be freed
  */
 void free_list(list_t *head)
 {
-	list_t *temp;
+	list_t *new;
 
 	while (head)
 	{
-		temp = head->next;
+		new = head->next;
 		free(head->str);
 		free(head);
-		head = temp;
+		head = new;
 	}
 }
